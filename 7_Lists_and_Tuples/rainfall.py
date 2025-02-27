52ff1d5a4b255e66348d4fc9d2137d979fab2fb8
George <Wes>
2025-02-27 05:01:29 -0600

    update

---
+++ 7_Lists_and_Tuples/rainfall.py
@@ -0,0 +1,40 @@
+"""
+    Calculates the total rainfall, average monthly rainfall, and months with highest/lowest rainfall
+    for 12 months of rainfall date entered by the user.
+    
+    Handles ValueError exceptions that are raised when the items that are entered by the user are 
+    converted to a number.
+
+    Args:
+        muffinCount (int): 
+
+    Outputs:
+	    Total, average, highest (name and amount), and lowest (name and amount) months of rainfall.
+        IOError and/or ValueError exceptions, when raised.
+"""
+
+# make a list of month names
+# make a list to get the 12 values
+# mean() function to get the average
+# min()/max() function will find greatest and least but I need the index
+# use index(MAX or MIN rainfall) to get the index that will = the month
+# print index(MAN or MIN) has the most rain, amount of rain
+
+'''
+a = [3, 5, 7, 2, 8, 1]
+
+# Find the maximum element
+mxv = max(a)
+
+# Find the position of maximum element
+mxp = a.index(mxv)
+
+# Find the minimum element
+mnv = min(a)
+
+# Find the position of minimum element
+mnp = a.index(mnv)
+
+print(mxp)
+print(mnp)
+'''
