f5e3862b120d22e2546fadb2136d4c752aa2fb2c
George <Wes>
2025-02-28 17:22:56 -0600

    Update 2/28/2025: finished Module 7, started Module 8.

--- 7_Lists_and_Tuples/rainfall.py
+++ 7_Lists_and_Tuples/rainfall.py
@@ -3,7 +3,7 @@
     for 12 months of rainfall date entered by the user.
     
     Handles ValueError exceptions that are raised when the items that are entered by the user are 
-    converted to a number.
+    converted.
 
     Args:
         muffinCount (int): 
@@ -14,73 +14,73 @@
 """
 import matplotlib.pyplot as plt
 
-
-### pushing update
-
 # make a tuple of the months
-months_list = ('January', 'February', 'March', 'April', 'May', 'June',
-            'July', 'August', 'September', 'October', 'November', 'December')
+months_list = ('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+                'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
 
-def getInputData():
+def get_input_data():
+    """A function to gather input and return a list"""
+    input_list = []
+    
+    try:
+        for month in months_list:
+            input_list.append(float(input("Enter the monthly rainfall for " + month + ":")))
+        return input_list
+    
+    except ValueError:
+        print("Value error: expected the number (integer)")
+
+def find_total(rf_list):
+    """a function to find and return the total of a list"""
+    total = 0
 
     try:
-        input_list = []
-        
-        for month in months_list:
-            input_list.append(input("Enter the monthly rainfall for " + month + ":"))
-
-        return input_list
-
+        for amount in rainfall_list:
+            total += float(amount)    
+        return total
+    
     except ValueError:
-        print("Value error: expected the name of a month (string)")
+     print("Value error: could not convert string to a float")
 
-def findAverage(rainfall_list):
+def find_average(rf_list):
+    """a function to find and return an average of a list"""
     average = 0
 
-    for amount in rainfall_list:
-        average += float(amount)
+    try:
+        for amount in rainfall_list:
+            average += float(amount)    
 
-    average = average / len(rainfall_list)
+        return average / len(rainfall_list)
+    
+    except ValueError:
+     print("Value error: could not convert string to a float")
 
-    return average
+# get input
+rainfall_list = get_input_data()
+# calculate total
+total_rainfall = find_total(rainfall_list)
+# calculate average
+average_rainfall = find_average(rainfall_list)
 
-rainfall_list = getInputData()
-average_rainfall = findAverage(rainfall_list)
+# grab the index of the rainfall minimum and maximum and use
+# those values to get the correct element from the months_list
+min_month = months_list[rainfall_list.index(min(rainfall_list))]
+max_month = months_list[rainfall_list.index(max(rainfall_list))]
 
-print(rainfall_list)
-print("Average rainfall was", round(average_rainfall, 2), "inches.")
-print("The minimum rainfall was", min(rainfall_list))
-print("The maximum rainfall was", max(rainfall_list))
+# print the required output
+print("Total rainfall was", round(total_rainfall, 2))
+print("Average rainfall was", round(average_rainfall, 2))
+print("The minimum rainfall was", min(rainfall_list), "inches in", min_month)
+print("The maximum rainfall was", max(rainfall_list), "inches in", max_month)
 
-# make a list to get the 12 values months.append(value)
-# mean() function to get the average
-# min()/max() function will find greatest and least but I need the index
-# use index(MAX or MIN rainfall) to get the index that will = the month
-# print index(MAN or MIN) has the most rain, amount of rain
-# plt.plot(avgRainFall, months)
-# plt.title('Average Rainfall')
-# plt.ylabel('Rainfall (inches))
-# plt.xlabel('Month')
-# plt.xlim() to add space?
-# plt.xticks(create a list of length Month, Months)
-# plt.grid(True)
+# trying some plotting 
+plt.plot(months_list, rainfall_list)
+plt.title('Yearly Average Rainfall by Month')
+plt.ylabel('Rainfall (inches)')
+plt.xlabel('Months')
+plt.xticks(rotation= 45, ha='right')
+plt.grid(True)
+plt.tight_layout()
 plt.show()
 
-'''
-a = [3, 5, 7, 2, 8, 1]
-
-# Find the maximum element
-mxv = max(a)
-
-# Find the position of maximum element
-mxp = a.index(mxv)
-
-# Find the minimum element
-mnv = min(a)
-
-# Find the position of minimum element
-mnp = a.index(mnv)
-
-print(mxp)
-print(mnp)
-'''
+print("Rainfall program complete.")
