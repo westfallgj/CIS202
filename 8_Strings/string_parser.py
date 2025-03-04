a4d93deb5dc65000b3d66b6a867dfdbe167a77cc
George Westfall <g.j.westfall@gmail.com>
2025-03-04 09:04:17 -0600

    Update to 8, 3/4/2025

--- 8_Strings/string_parser.py
+++ 8_Strings/string_parser.py
@@ -17,34 +17,11 @@
 """
 INPUT_FILE = "test_file.txt"
 
-def main():
+names_list = ()
+sorted_list = ()
 
-    names_list = ()
-    reversed_names_list = ()
-    alpha_names_list = ()
-    sorted_names_list = ()    
-    
-
-    names_list = read_test_input_file()
-    print("NAMES LIST")
-    print(names_list)
-
-    reversed_names_list = reverse_name(names_list)
-    print("Reversed NAMES LIST")
-    print(reversed_names_list)
-    
-    alpha_names_list = alpha_order(reversed_names_list)
-    print("alpha_sort process returns")
-    print(alpha_names_list)
-
-    reversed_names_list = reverse_name(alpha_names_list)
-    print("reverse list again")
-    print(reversed_names_list)
-
-
-def read_test_input_file():
-    """Gather input to  a list and return."""
-
+def read_input_file():
+    """Gather/Stript/Split input to a list and return."""
     try:
         # Open input file
         input_file = open(INPUT_FILE, 'r')
@@ -63,26 +40,16 @@ def read_test_input_file():
     except IOError:
         print("IOError: the input file cannot be found.")
  
-def reverse_name(list_to_sort):
-    """Sorts a list of names by the last name"""
-    full_names_list = []
-    this_name = []
+def get_last_name(name_list):
+    """Creates/Returns a last name list"""
+    return name_list.split()[-1]
 
-    for full_name in list_to_sort:
-        this_name = full_name.split()
-        full_names_list.append(this_name[1] + " " + this_name[0])
-    
-    return full_names_list
+# Call to read input file
+names_list = read_input_file()
+print("Names List", names_list)
 
-def alpha_order(unordered_list):
-    """ something """
-    ordered_list = []
+# sort names, using the get_last_name function in the iteration
+sorted_names = sorted(names_list, key=get_last_name)
 
-    for names in unordered_list:
-        tokens = names.split(" ")
-        ordered_list.append(tokens[1]+ " " + tokens[0])
-
-    return ordered_list
-
-# Call main function
-main()
+for name in sorted_names:
+    print(name)
